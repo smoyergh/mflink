66ac762fb3ef723ed9a8879c466664eb439de770
Steven Moyer <steve@mflink.com>
2023-07-09 19:31:59 -0500

    Play with VitePress by creating a single page for mflink.com.

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "mflink",
+  description: "Moyer Family Link",
+  lang: 'en-US',
+  appearance: "dark",
+  base: '/mflink/',
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+//    nav: [
+//       { text: 'Home', link: '/' },
+//    ],
+//
+//    socialLinks: [
+//      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+//    ]
+  }
+})
---
+++ docs/index.md
@@ -0,0 +1,9 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "mflink"
+  text: "Moyer Family Link"
+---
+
